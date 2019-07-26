8228b2b87bed6ec6925de3e42a9d7cdd8c30e581
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-07-26 20:07:38 +0200

    add  all changes made on week3-jsquery this evening

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+var name = document.getElementById("mail");
+var email = document.getElementById("name");
+var message = document.getElementById("info");
+ console.log(name);
+ document.getElementById("button")
+
+
+
+
+
 $(document).ready(function(){
     $("#create,#path").click(function(){
      $(".hide").toggle();
@@ -8,9 +18,20 @@ $(document).ready(function(){
      $(".hidden").toggle();
      $("#see").toggle();
     });
+
     $("#produce,#get").click(function(){
     $(".hidding").toggle();
     $("#get").toggle();
     });
+
+    $('#w1').hide();
+    $('# p1').animate({opacity:1});
+    $(".p1").hover(function(){
+         $(this).stop() .animate({opacity:.4},200);
+        $('#w1').fadeIn();
+    }, function(){
+        $(this).stop()
+         .animate({opacity:1},500);
+        $('#w1').fadeOut();
+    });
 });
-$

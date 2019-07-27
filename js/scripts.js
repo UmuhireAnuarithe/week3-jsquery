36633a994b4a78b1abbeca0d6ed5e0b799a84103
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-07-27 19:07:20 +0200

    add new changes on css js and index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
-function  comment(){
-var name = document.getElementById("name");
+function comment(){
+var name = document.getElementById("name").val;
 var email = document.getElementById("mail");
 var message = document.getElementById("info");
  console.log(name);
  document.getElementById("button").checked;
-//  alert("Thank you " + " ! " + " name:" + " your message recieved")
 
+//  }
+// //  if(name=="" || email=="" || message==""){
+//      alert(name,email,message,required );
+//  }
+//  else
+{
+ alert("Thank you " + " ! " + " name:" + " your message recieved");
 }
-
-
+}
 
 $(document).ready(function(){
     $("#create,#path").click(function(){
@@ -24,16 +29,46 @@ $(document).ready(function(){
     $("#produce,#get").click(function(){
     $(".hidding").toggle();
     $("#get").toggle();
+     });
+
+   $('.p1').hide();
+   $('.pp1').animate({opacity:1});
+   $(".pp1").hover(function(){
+   $(this).stop() .animate({opacity:.4},200);
+   $('.p1').fadeIn();
+   }, function(){
+   $(this).stop().animate({opacity:1},500);
+   $('.p1').fadeOut();   
     });
 
-    $('#w1').hide();
-    $('# p1').animate({opacity:1});
-    $(".p1").hover(function(){
-         $(this).stop() .animate({opacity:.4},200);
-        $('#w1').fadeIn();
-    }, function(){
-        $(this).stop()
-         .animate({opacity:1},500);
-        $('#w1').fadeOut();
+    $('.p2').hide();
+   $('.pp1').animate({opacity:1});
+   $(".pp1").hover(function(){
+   $(this).stop() .animate({opacity:.4},200);
+   $('.p2').fadeIn();
+   }, function(){
+   $(this).stop().animate({opacity:1},500);
+   $('.p2').fadeOut();   
     });
+
+
+    $('.p3').hide();
+   $('.pp1').animate({opacity:1});
+   $(".pp1").hover(function(){
+   $(this).stop() .animate({opacity:.4},200);
+   $('.p3').fadeIn();
+   }, function(){
+   $(this).stop().animate({opacity:1},500);
+   $('.p3').fadeOut();   
+    });
+
+    $('.p4').hide();
+    $('.pp1').animate({opacity:1});
+    $(".pp1").hover(function(){
+    $(this).stop() .animate({opacity:.4},200);
+    $('.p4').fadeIn();
+    }, function(){
+    $(this).stop().animate({opacity:1},500);
+    $('.p4').fadeOut();   
+     });
 });

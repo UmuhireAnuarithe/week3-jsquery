19bd11872c36fc043948ecc777d095001d3669d1
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-07-27 20:15:48 +0200

    add new changes on css js and index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,3 @@
-function comment(){
-var name = document.getElementById("name").val;
-var email = document.getElementById("mail");
-var message = document.getElementById("info");
- console.log(name);
- document.getElementById("button").checked;
-
-//  }
-// //  if(name=="" || email=="" || message==""){
-//      alert(name,email,message,required );
-//  }
-//  else
-{
- alert("Thank you " + " ! " + " name:" + " your message recieved");
-}
-}
 
 $(document).ready(function(){
     $("#create,#path").click(function(){
@@ -42,8 +26,8 @@ $(document).ready(function(){
     });
 
     $('.p2').hide();
-   $('.pp1').animate({opacity:1});
-   $(".pp1").hover(function(){
+   $('.pp2').animate({opacity:1});
+   $(".pp2").hover(function(){
    $(this).stop() .animate({opacity:.4},200);
    $('.p2').fadeIn();
    }, function(){
@@ -53,8 +37,8 @@ $(document).ready(function(){
 
 
     $('.p3').hide();
-   $('.pp1').animate({opacity:1});
-   $(".pp1").hover(function(){
+   $('.pp3').animate({opacity:1});
+   $(".pp3").hover(function(){
    $(this).stop() .animate({opacity:.4},200);
    $('.p3').fadeIn();
    }, function(){
@@ -63,12 +47,67 @@ $(document).ready(function(){
     });
 
     $('.p4').hide();
-    $('.pp1').animate({opacity:1});
-    $(".pp1").hover(function(){
+    $('.pp8').animate({opacity:1});
+    $(".pp8").hover(function(){
     $(this).stop() .animate({opacity:.4},200);
     $('.p4').fadeIn();
     }, function(){
     $(this).stop().animate({opacity:1},500);
     $('.p4').fadeOut();   
      });
+
+    $('.work1').hide();
+    $('.pp4').animate({opacity:1});
+    $(".pp4").hover(function(){
+    $(this).stop() .animate({opacity:.4},200);
+    $('.work1').fadeIn();
+    }, function(){
+    $(this).stop().animate({opacity:1},500);
+    $('.work1').fadeOut();   
+     });
+
+     $('.work2').hide();
+    $('.pp5').animate({opacity:1});
+    $(".pp5").hover(function(){
+    $(this).stop() .animate({opacity:.4},200);
+    $('.work2').fadeIn();
+    }, function(){
+    $(this).stop().animate({opacity:1},500);
+    $('.work2').fadeOut();   
+     });
+
+     $('.work3').hide();
+    $('.pp6').animate({opacity:1});
+    $(".pp6").hover(function(){
+    $(this).stop() .animate({opacity:.4},200);
+    $('.work3').fadeIn();
+    }, function(){
+    $(this).stop().animate({opacity:1},500);
+    $('.work3').fadeOut();   
+     });
+
+     $('.work4').hide();
+    $('.pp7').animate({opacity:1});
+    $(".pp7").hover(function(){
+    $(this).stop() .animate({opacity:.4},200);
+    $('.work4').fadeIn();
+    }, function(){
+    $(this).stop().animate({opacity:1},500);
+    $('.work4').fadeOut();   
+     });
+
+     $("#button").click(function () {
+        var names = $("#user").val();
+        var email = $("#mail").val();
+        var message = $("#info").val();
+        if ((names === "") || (email === "") || (message === "")) {
+            alert("Please, fill the form! before you submit");
+        }
+        else {
+            alert( "Hi " + names + ", we have received your message. Thank you to communicate!");
+        }
+
+    });
+
 });
+
